e65aa47851a0fe75ba14d45a985b2b6c6675dcc2
Adam Bertram <adbertram@gmail.com>
2020-01-08 16:31:06 -0600

    addded connect-azure.ps1

---
+++ connect-azure.ps1
@@ -0,0 +1,15 @@
+$azureAdAppId = '21f9ea0e-b668-4cc2-93e0-6f6fd338d57f'
+$subscriptionId = '1427e7fb-a488-4ec5-be44-30ac10ca2e95'
+$tenantId = '11376bd0-c80f-4e99-b86f-05d17b73518d'
+$spPrinPw = "$(ServerAutomationDemo-AppPw)"
+
+$password = ConvertTo-SecureString $spPrinPw -AsPlainText -Force
+$credential = New-Object System.Management.Automation.PSCredential ($azureAdAppId, $password)
+
+$connectAzParams = @{
+    ServicePrincipal = $true
+    SubscriptionId   = $subscriptionId
+    Tenant           = $tenantId
+    Credential       = $credential
+}
+Connect-AzAccount @connectAzParams
