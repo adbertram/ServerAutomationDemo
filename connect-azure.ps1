71cbd9264c51aadfdb1348eb8117917d97580fd5
Adam Bertram <adbertram@gmail.com>
2020-01-08 16:31:35 -0600

    updates

--- connect-azure.ps1
+++ connect-azure.ps1
@@ -1,3 +1,5 @@
+Install-Module -Name Az.Accounts, Az.Resources -Force -SkipPublisherCheck
+
 $azureAdAppId = '21f9ea0e-b668-4cc2-93e0-6f6fd338d57f'
 $subscriptionId = '1427e7fb-a488-4ec5-be44-30ac10ca2e95'
 $tenantId = '11376bd0-c80f-4e99-b86f-05d17b73518d'
