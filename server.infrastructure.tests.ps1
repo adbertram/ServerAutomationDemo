e3707ebc430eeaf5e2f8c3f276d4e7fcfd6b2e7e
Adam Bertram <adbertram@gmail.com>
2020-01-09 12:41:13 -0600

    updates

--- server.infrastructure.tests.ps1
+++ server.infrastructure.tests.ps1
@@ -2,11 +2,15 @@
 param(
     [Parameter(Mandatory)]
     [ValidateNotNullOrEmpty()]
-    [string]$Hostname
+    [string]$ArmDeploymentJsonOutput
 )
 
+## Parse the ARM output
+$ArmDeploymentOutput = $ArmDeploymentJsonOutput | convertfrom-json
+
+## Run the tests
 describe 'Network Connnectivity' {
     it 'the VM has RDP/3389 open' {
-        Test-Connection -TCPPort 3389 -TargetName $Hostname -Quiet | should -Be $true
+        Test-Connection -TCPPort 3389 -TargetName $ArmDeploymentOutput.hostname.value -Quiet | should -Be $true
     }
 }
