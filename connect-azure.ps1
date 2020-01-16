d48d9ab1bbf02b8fca0799c5d93df13cdd4d4ef2
Adam Bertram <adbertram@gmail.com>
2020-01-16 13:18:26 -0600

    updates

--- connect-azure.ps1
+++ connect-azure.ps1
@@ -2,23 +2,30 @@
 param (
     [Parameter(Mandatory)]
     [ValidateNotNullOrEmpty()]
-    [string]$ServicePrincipalPassword
-)
+    [string]$ServicePrincipalPassword,
 
+    [Parameter(Mandatory)]
+    [ValidateNotNullOrEmpty()]
+    [string]$SubscriptionId,
+
+    [Parameter(Mandatory)]
+    [ValidateNotNullOrEmpty()]
+    [string]$ApplicationId,
+
+    [Parameter(Mandatory)]
+    [ValidateNotNullOrEmpty()]
+    [string]$TenantId
+)
 
 Install-Module -Name Az.Accounts, Az.Resources -Force -SkipPublisherCheck
 
-$azureAdAppId = '21f9ea0e-b668-4cc2-93e0-6f6fd338d57f'
-$subscriptionId = '1427e7fb-a488-4ec5-be44-30ac10ca2e95'
-$tenantId = '11376bd0-c80f-4e99-b86f-05d17b73518d'
-
 $password = ConvertTo-SecureString $ServicePrincipalPassword -AsPlainText -Force
-$credential = New-Object System.Management.Automation.PSCredential ($azureAdAppId, $password)
+$credential = New-Object System.Management.Automation.PSCredential ($ApplicationId, $password)
 
 $connectAzParams = @{
     ServicePrincipal = $true
-    SubscriptionId   = $subscriptionId
-    Tenant           = $tenantId
+    SubscriptionId   = $SubscriptionId
+    Tenant           = $TenantId
     Credential       = $credential
 }
 Connect-AzAccount @connectAzParams
